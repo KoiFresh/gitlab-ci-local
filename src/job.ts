1c7db1d7e7b23177e137152441286b1ca51e755c
Kai Mayer <kai@mayer.sh>
2024-11-07 12:15:16 +0100

    feat: add option to specify interactive jobs via cli

--- src/job.ts
+++ src/job.ts
@@ -385,7 +385,9 @@ export class Job {
     }
 
     get interactive (): boolean {
-        return this.jobData["gclInteractive"] || false;
+        return this.jobData["gclInteractive"]
+            || this.argv.interactiveJobs.indexOf(this.name) >= 0
+            || false;
     }
 
     get injectSSHAgent (): boolean {
